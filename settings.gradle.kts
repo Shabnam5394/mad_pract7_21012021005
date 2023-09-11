938075c42eea825a3bf27248425d37f6327be99e
student <student@gnu.ac.in>
2023-09-11 10:41:07 +0530

    p7

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "mad_pract7_21012021005"
+include(":app")
+ 
